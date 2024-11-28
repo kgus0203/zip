2934917b85d298abc5949de67a454144cfb2aa2b
kgus0203 <80197853+kgus0203@users.noreply.github.com>
2024-11-29 01:09:19 +0900

    Update test.py

--- test.py
+++ test.py
@@ -111,7 +111,7 @@ def setting_page():
     view.render_user_profile()
     view.render_alarm_settings()
 
-    theme_manager = setting.ThemeManager()
+    theme_manager = ThemeManager()
     theme_manager.render_button()
 
     view.render_posts()
