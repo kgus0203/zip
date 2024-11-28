0b4ffb8fd254d26c1b7326a63a295ecf6efb3499
kgus0203 <80197853+kgus0203@users.noreply.github.com>
2024-11-28 19:03:20 +0900

    Update test.py

--- test.py
+++ test.py
@@ -992,6 +992,11 @@ class PostManager:
                     post = posts[i + j]  # 현재 포스트 데이터
                     with col:
                         st.subheader(post.p_title)
+                        self.fetch_location_data(post.p_id)
+
+                        # 위치 데이터가 존재할 때만 지도 생성
+                        if self.locations_df is not None and not self.locations_df.empty:
+                            self.create_location_name()
 
                         # 이미지 출력 (있는 경우)
                         if post.p_image_path:
@@ -1001,14 +1006,8 @@ class PostManager:
                             except Exception as e:
                                 st.error(f"이미지를 불러오는 데 실패했습니다: {e}")
                         else:
-                            
-                            self.fetch_location_data(post.p_id)
 
-                            # 위치 데이터가 존재할 때만 지도 생성
-                            if self.locations_df is not None and not self.locations_df.empty:
-                                self.create_location_name()
-                                self.display_map()
-                        with st.expander('더보기'):
+                            with st.expander('더보기'):
                                     self.display_post(post.p_id)
 
 #----------------------------------------------------카테고리 -----------------------------
