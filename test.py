495968c0467051d1f375f5261db3868af6a99ff2
kgus0203 <80197853+kgus0203@users.noreply.github.com>
2024-11-29 12:35:48 +0900

    Update test.py

--- test.py
+++ test.py
@@ -49,7 +49,8 @@ class Page:
             'View Post': self.turn_pages.view_post,
             'Setting': self.turn_pages.setting_page,
             'User manager': self.turn_pages.usermanager_page,
-            'ID PW 변경': self.turn_pages.id_pw_change_page,
+            'ID PW 변경': self.turn_pages.id_pw_change_page,'
+            'Upload Post' : self.turn_pages.upload_post,
         }
 
         # 현재 페이지 렌더링
